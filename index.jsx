46eff3c35470d8f19670dd226f0233a11b48a80e
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2025-02-04 10:24:16 +0000

    Create index.jsx

---
+++ index.jsx
@@ -0,0 +1,29 @@
+export const Footer = () => {
+  return (<footer className='footer-container'>
+  <section>
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Skills</a></li>
+  </ul>
+  </section>
+
+  <section>
+  <ul>
+    <li><a href="#">Experience</a></li>
+    <li><a href="#">Portfolio</a></li>
+    <li><a href="#">Contact</a></li>
+  </ul>
+  </section>
+
+  <section>
+    <ul>
+      <li><a href="#">LinkedIn</a></li>
+      <li><a href="#">Facebook</a></li>
+      <li><a href="#">GitHub</a></li>
+    </ul>
+  </section>
+
+  <p className="copyright">&copy; Derek Dhammaloka 2025</p>
+  </footer>);
+};
